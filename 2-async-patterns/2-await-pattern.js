9142cd6d960ec9ce57bc72f513a5db7f7d2cf87b
kareemah <ajimobikareemah@gmail.com>
2022-09-06 14:29:56 +0100

    add more gibberish

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -1,18 +1,61 @@
-const { readFile } = require('fs');
-const { reject } = require('lodash');
+const { readFile, writeFile } = require('fs');
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf-8', (error, res) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(res);
-      }
-    });
-  });
+const start = async () => {
+  try {
+    const first = await readFilePromise('../content/first.txt', 'utf-8');
+    const second = await readFilePromise('../content/second.txt', 'utf-8');
+    const res = first + '\n' + second;
+    await writeFilePromise('../content/mind-grenade.txt', res);
+  } catch (error) {
+    console.log(error);
+  }
 };
+start();
+// const start = async () => {
+//   try {
+//     const first = await getText('../content/first.txt');
+//     const second = await getText('../content/second.txt');
+//     const result = first + '\n' + second;
+//     console.log(result);
+//     writeFile('../content/result_async.txt', result, (err, res) => {
+//       if (err) {
+//         console.log(error);
+//         return;
+//       }
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-getText('./content/first.txt')
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf-8', (error, res) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(res);
+//       }
+//     });
+//   });
+// };
+
+// using the good old promises
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf-8', (error, res) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(res);
+//       }
+//     });
+//   });
+// };
+
+// getText('./content/first.txt')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
